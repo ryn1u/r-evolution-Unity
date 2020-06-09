a2dc9d2ecb33d2a56cd92d7c400d586ed67aebc9
ryn1u <ryniu98@gmail.com>
2020-06-09 16:50:33 +0200

    ver09062020
    Added custoom editors for Animal Data and Animal Ability Data

---
+++ Assets/Editor/AnimalAbilityEditor.cs
@@ -0,0 +1,157 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Runtime.CompilerServices;
+using System.Linq;
+using NUnit.Framework;
+
+public class AnimalAbilityEditor : EditorWindow
+{
+    [MenuItem("Window/Animal Ability Editor")]
+    public static void OpenWindow()
+    {
+        GetWindow<AnimalAbilityEditor>("Animal Data Editor");
+    }
+
+    Vector2 scrollPos;
+    private List<AbilityData> abilityDatabase;
+    private List<AnimalData> animalDatabase;
+    private Dictionary<string, AnimalData> animalList;
+    private AnimalData currentAnimal;
+    private AbilityData currentAbility;
+    private string[] names;
+
+    private bool hasAbility
+    {
+        get
+        {
+            if(currentAnimal != null && currentAbility != null)
+            {
+                return currentAnimal.abilities.Contains(currentAbility);
+            }
+            else
+            {
+                return false;
+            }
+        }
+        set
+        {
+            if (currentAnimal != null && currentAbility != null)
+            {
+                if (value && !hasAbility)
+                {
+                    currentAnimal.abilities.Add(currentAbility);
+                }
+                else if(!value && hasAbility)
+                {
+                    currentAnimal.abilities.Remove(currentAbility);
+                }
+            }
+        }
+    }
+
+    int index = 0;
+    private void OnGUI()
+    {
+        float windowWidth = position.width;
+        float nameFieldWidth = windowWidth * 0.6f;
+        float elseFieldWidth = (windowWidth - nameFieldWidth) / 3;
+        //str, def, pwr, will, agi, hp
+        //name, id
+        scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+        index = EditorGUILayout.Popup(index, names);
+
+        currentAnimal = animalList[names[index]];
+        titleContent = new GUIContent(currentAnimal.animalName + " Abiliy Editor");
+
+        foreach(AbilityData ability in abilityDatabase)
+        {
+            currentAbility = ability;
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField(ability.abilityName, GUILayout.Width(elseFieldWidth));
+            EditorGUILayout.LabelField(ability.description, GUILayout.Width(nameFieldWidth));
+            hasAbility = EditorGUILayout.Toggle(hasAbility);
+            EditorGUILayout.EndHorizontal();
+        }
+
+        EditorGUILayout.EndScrollView();
+    }
+    private void OnEnable()
+    {
+        abilityDatabase = GetAbilityDatabase();
+        animalDatabase = GetAnimalDatabase();
+        animalList = GetAnimalList(animalDatabase);
+        names = GetAnimalNamesArray(animalDatabase);
+    }
+    private void OnProjectChange()
+    {
+        abilityDatabase = GetAbilityDatabase();
+        animalDatabase = GetAnimalDatabase();
+        animalList = GetAnimalList(animalDatabase);
+        names = GetAnimalNamesArray(animalDatabase);
+    }
+
+    public static List<AbilityData> GetAbilityDatabase()
+    {
+        List<AbilityData> output = new List<AbilityData>();
+        string[] guids = AssetDatabase.FindAssets("t:AbilityData");
+        foreach (string guid in guids)
+        {
+            string path = AssetDatabase.GUIDToAssetPath(guid);
+            AbilityData data = AssetDatabase.LoadAssetAtPath<AbilityData>(path);
+            output.Add(data);
+            Debug.Log("abi data: " + data.abilityName);
+        }
+        return output;
+    }
+
+    public static Dictionary<string, AnimalData> GetAnimalList(List<AnimalData> database)
+    {
+        Dictionary<string, AnimalData> output = new Dictionary<string, AnimalData>();
+
+        foreach(AnimalData data in database)
+        {
+            output.Add(data.animalName, data);
+        }
+
+        return output; ;
+    }
+    public static List<AnimalData> GetAnimalDatabase()
+    {
+        List<AnimalData> output = new List<AnimalData>();
+        string[] guids = AssetDatabase.FindAssets("t:AnimalData");
+        foreach (string guid in guids)
+        {
+            string path = AssetDatabase.GUIDToAssetPath(guid);
+            AnimalData data = AssetDatabase.LoadAssetAtPath<AnimalData>(path);
+            output.Add(data);
+        }
+        return output;
+    }
+    public static string[] GetAnimalNamesArray(List<AnimalData> database)
+    {
+        List<string> names = new List<string>();
+        foreach(AnimalData a in database)
+        {
+            names.Add(a.animalName);
+        }
+        return names.ToArray();
+    }
+
+    public void SetCurrentAnimal(AnimalData animal)
+    {
+        if(names.Count() != 0)
+        {
+            currentAnimal = animal;
+            for(int i = 0; i < names.Count(); i++)
+            {
+                if(animal.animalName == names[i])
+                {
+                    index = i;
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Editor/AnimalDataEditor.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Runtime.CompilerServices;
+
+public class AnimalDataEditor : EditorWindow
+{
+    [MenuItem("Window/Animal Data Editor")]
+    public static void OpenWindow()
+    {
+        GetWindow<AnimalDataEditor>("Animal Data Editor");
+    }
+
+    string newName = "";
+    Vector2 scrollPos;
+    private List<AnimalData> animalDatabase;
+    private void OnGUI()
+    {
+        float windowWidth = position.width;
+        float nameFieldWidth = windowWidth * 0.3f;
+        float elseFieldWidth = (windowWidth - nameFieldWidth) / 8;
+        //str, def, pwr, will, agi, hp
+        //name, id
+        scrollPos = EditorGUILayout.BeginScrollView(scrollPos);
+
+        Rect topLabels = EditorGUILayout.BeginHorizontal("Label", GUILayout.MinWidth(10),GUILayout.ExpandWidth(true));
+        EditorGUILayout.LabelField("Id", GUILayout.Width(elseFieldWidth));
+        EditorGUILayout.LabelField("Name", GUILayout.Width(nameFieldWidth));
+        EditorGUILayout.LabelField("STR", GUILayout.Width(elseFieldWidth));
+        EditorGUILayout.LabelField("DEF", GUILayout.Width(elseFieldWidth));
+        EditorGUILayout.LabelField("PWR", GUILayout.Width(elseFieldWidth));
+        EditorGUILayout.LabelField("WILL", GUILayout.Width(elseFieldWidth));
+        EditorGUILayout.LabelField("AGI", GUILayout.Width(elseFieldWidth));
+        EditorGUILayout.LabelField("HP", GUILayout.Width(elseFieldWidth));
+        EditorGUILayout.EndHorizontal();
+
+        foreach(AnimalData entry in animalDatabase)
+        {
+            EditorGUILayout.BeginHorizontal();
+            entry.id = EditorGUILayout.IntField(entry.id, GUILayout.Width(elseFieldWidth/2));
+            entry.animalName = EditorGUILayout.TextField(entry.animalName, GUILayout.Width(nameFieldWidth));
+            entry.STR = EditorGUILayout.IntField(entry.STR, GUILayout.Width(elseFieldWidth));
+            entry.DEF = EditorGUILayout.IntField(entry.DEF, GUILayout.Width(elseFieldWidth));
+            entry.PWR = EditorGUILayout.IntField(entry.PWR, GUILayout.Width(elseFieldWidth));
+            entry.WILL = EditorGUILayout.IntField(entry.WILL, GUILayout.Width(elseFieldWidth));
+            entry.AGI = EditorGUILayout.IntField(entry.AGI, GUILayout.Width(elseFieldWidth));
+            entry.HP = EditorGUILayout.IntField(entry.HP, GUILayout.Width(elseFieldWidth));
+            if(GUILayout.Button("Abilities"))
+            {
+                AnimalAbilityEditor.OpenWindow();
+                AnimalAbilityEditor abiEditor = GetWindow<AnimalAbilityEditor>();
+                abiEditor.SetCurrentAnimal(entry);
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+
+        EditorGUILayout.BeginHorizontal(GUILayout.Width(nameFieldWidth + elseFieldWidth/2));
+        newName = EditorGUILayout.TextField(newName, GUILayout.Width(nameFieldWidth));
+        if(GUILayout.Button("+"))
+        {
+            CreateNewAnimal(newName);
+            newName = "";
+        }
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.EndScrollView();
+    }
+    private void OnEnable()
+    {
+        animalDatabase = GetAnimalDatabase();
+    }
+    private void OnProjectChange()
+    {
+        animalDatabase = GetAnimalDatabase();
+    }
+
+    public void CreateNewAnimal(string name)
+    {
+        AnimalData animalData = ScriptableObject.CreateInstance<AnimalData>();
+        animalData.animalName = name;
+        AssetDatabase.CreateAsset(animalData, "Assets/Animals/" + name + ".asset");
+    }
+    public List<AnimalData> GetAnimalDatabase()
+    {
+        List<AnimalData> output = new List<AnimalData>();
+        string[] guids = AssetDatabase.FindAssets("t:AnimalData");
+        foreach(string guid in guids)
+        {
+            string path = AssetDatabase.GUIDToAssetPath(guid);
+            AnimalData data = AssetDatabase.LoadAssetAtPath<AnimalData>(path);
+            output.Add(data);
+        }
+
+        return output;
+    }
+}
